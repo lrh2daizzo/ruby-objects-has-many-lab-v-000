80686c79210fa2dc518961f23f16e0477b0be707
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 17:06:35 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,5 +8,17 @@ class Author
   def posts
     Post.all.select {|post| post.author == self}
   end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    Post.new(title).tap {|post| self.add_post(post)}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
   
 end
